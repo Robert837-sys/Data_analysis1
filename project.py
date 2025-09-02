f3adfa18267af7ddf80a98bcfdafaa75a0269a5c
Robert837 -sys <robertagie2022@gmail.com>
2025-09-02 11:07:52 +0300

    my first commit

---
+++ project.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np
+
+df = pd.read_excel('sales.xlsx',engine='openpyxl', nrows=10)
+# This data lacks Retails and wholesale columns but in case they were there, this is the formulae to calculate total using pandas in python
+# df['Total'] = df['Retails'] + df['Wholesale']
+
+
+# let's say you are given Actual date column and you want to extract say month, this can be done by the following formulae
+# df['Month']=df['Actual Date'].dt.month
+
+# to extract month and year at the same time use the following formulae
+# df['Month_ear']=df['Actual Date'].dt.strftime()
+print(df.head())
