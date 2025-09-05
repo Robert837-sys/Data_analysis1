f5a6c75facd2160a4a9347761fd4a60c2b88d5f5
Robert837 -sys <robertagie2022@gmail.com>
2025-09-05 13:08:47 +0300

    final pandas project

--- clean_phoneNumbers12.py
+++ clean_phoneNumbers12.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 df=pd.read_excel('Customer Call List.xlsx')
 df.drop(columns="Not_Useful_Column",inplace=True)
+df.drop_duplicates(inplace=True)
+df["Last_Name"]=df["Last_Name"].str.strip("123._/")
 
 df['Phone_Number']=df['Phone_Number'].astype(str)
 df['Phone_Number']=df['Phone_Number'].str.replace('[^0-9]','',regex=True)
@@ -11,6 +13,22 @@ def format_phone(x):
   else:
     return ''
 df['Phone_Number']=df['Phone_Number'].apply(format_phone)
-print(df['Address'].str.split(',',n=2,expand=True))
+df['Address'].str.split(',',n=2,expand=True)
 df[["street_Address","State","Zip_Code"]]=df['Address'].str.split(',',n=2,expand=True)
+df['Paying Customer'] = df['Paying Customer'].str.replace('Yes', 'Y', regex=True)
+df['Paying Customer'] = df['Paying Customer'].str.replace('No', 'N', regex=True)
+df['Do_Not_Contact'] = df['Do_Not_Contact'].str.replace('Yes', 'Y', regex=True)
+df['Do_Not_Contact'] = df['Do_Not_Contact'].str.replace('No', 'N', regex=True)
+df.replace('N/a', '', regex=True, inplace=True)
+df.fillna('', inplace=True)
+
+for x in df.index:
+  if df.loc[x,"Do_Not_Contact"]=='Y':
+    df.drop(x,inplace=True)
+for x in df.index:
+  if df.loc[x,"Phone_Number"]=='':
+    df.drop(x,inplace=True)
+
+df.reset_index(drop=True, inplace=True)
+# drop=True prevents old column from being added to the dataframe
 print(df.to_string())
--- pandas_test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel('Customer Call List.xlsx')
-
-print(df.columns)
